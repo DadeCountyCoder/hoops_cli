85c3f77b78666365451060f4efdf3aa47d2868fd
Alfredo Diaz <mail4alfredo@gmail.com>
2020-01-14 11:42:47 -0500

    1st hardcode

---
+++ NOTES.md
@@ -0,0 +1,10 @@
+PLanning Cli Project
+
+1.Think of something i would personally like
+2.make github repo
+3.make a gem
+4.make structure
+5.make sure it runs and prints 'working' from cli.rb by running bin/run
+6.hardcode project
+7.start scraping
+8.check cli checklist off learn site
--- lib/hoops_cli/cli.rb
+++ lib/hoops_cli/cli.rb
@@ -1,9 +1,26 @@
 module HoopsCli
     class CLI
-        
+
         def call
-            puts 'working'
+            puts "Welcome Heat Nation!"
+            current_standing
+            roster
         end
 
-    end 
+        def current_standing
+            puts 'record 27-12, currently 3rd place eastern conference'
+        end
+
+        def roster
+            puts 'starting_lineup
+            pg_kendrick_nunn
+            sg_Jimmy Butler
+            sf_Duncan Robinson
+            pf_Meyers Leonard
+            c_Bam Adebayo'
+
+        end
+
+
+    end
 end
