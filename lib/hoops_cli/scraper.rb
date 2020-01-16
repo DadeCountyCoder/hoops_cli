a3aae5024224abebf824c9cab649ebc7b970d86c
Alfredo Diaz <mail4alfredo@gmail.com>
2020-01-16 15:42:00 -0500

    setting up scrape

--- lib/hoops_cli/scraper.rb
+++ lib/hoops_cli/scraper.rb
@@ -1,5 +1,13 @@
+require "hoops_cli/nba"
+require 'pry'
+
 module HoopsCli
     class Scraper
+
+        def self.scrape
+            site = ""
+            page = Nokogiri:HTML(open(site))
+                page.css
 
 
     end
