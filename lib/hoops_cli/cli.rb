6b3b963a4050949d9618a7f49a159b2a0d4a0af9
Alfredo Diaz <mail4alfredo@gmail.com>
2020-01-19 18:09:53 -0500

    code flowing/scrapin

--- lib/hoops_cli/cli.rb
+++ lib/hoops_cli/cli.rb
@@ -1,10 +1,11 @@
 class HoopsCli::CLI
   
   def call 
+     puts
      puts "Welcome to the Heat Nation experience!"
      puts
      HoopsCli::Scraper.scrape
-     puts "Please enter a number for you favorite player!"
+     puts "Please enter a number for info on any player from the roster below!"
      puts
      players
      team
@@ -16,17 +17,16 @@ class HoopsCli::CLI
     puts "#{index}. #{object.name}"
     end
   end
-  
+
+
   def team
     puts
-    puts "Type the number of your Player"
-    puts 
     puts "You can type exit at anytime to leave the program"
     input = ""
     while input != "exit"
       input = gets.chomp
-      
-        
+
+
       if (1..14).include?(input.to_i)
         @chosen_player = HoopsCli::Nba.all[input.to_i - 1]
         puts
@@ -36,28 +36,50 @@ class HoopsCli::CLI
         puts "For #{@chosen_player.name}'s game statistics go to:"
         puts "#{@chosen_player.url}"
         puts
-        puts "Type 'r' to view the roster again:"
+        puts "Type 'roster' to view the roster again:"
         puts "Or type the number of another player:"
         puts
         puts "You can type 'exit' at any time to leave the program"
-        elsif input.downcase == "r"
+        elsif input.downcase == "roster"
           players
           team
         elsif input.downcase == "exit"
            puts
-           puts "I hope you enjoyed yourself!"
+           puts "And thats the way it is!"
           break
         else
           puts
-          puts "Sorry! Please enter a specific number from the roster!"
+          puts "Sorry! Please enter a specific number from the choices given!"
           players
           team
         end
       end
-      # puts "Adios!"
+       puts "Signing off!"
       exit
     end 
 end
 
+# def team
+#   puts
+#   puts "You can type exit at anytime to leave the program"
+#   input = ""
+#   while input != "exit"
+#     input = gets.chomp
+#   if input == 'exit'
+#     puts 'See you next time!'
+#   elsif input.to_i.is_a? Numeric
+#     if input.to_i.between?(1, 14)
+#       # scrape(input.to_i)
+#     else
+#       puts 'Please info on one of these players!'
+      
+#     end
+#     menu
+#   else
+#     puts 'Select a number or type exit'
+    
+#     menu
+#   end
+# end
 
         
